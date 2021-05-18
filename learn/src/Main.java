749b5d729d8281b1aafcb4bc34bf41949768e46b
enginnerFrankLiu <enginnerFrankLiu@icloud.com>
2021-05-18 22:13:49 +0800

    log info,

---
+++ learn/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void helloInfo() {
+        System.out.println("hello java.");
+        System.out.println("fuck the java.");
+    }
+    public static void main(String[] args) throws Exception {
+        System.out.format("application start.").println();
+        helloInfo();
+        System.out.format("application end.");
+    }
+
+}
