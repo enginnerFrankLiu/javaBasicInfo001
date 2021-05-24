81db4283a9dcb65ff70acdfb87b4a4668e9b202d
enginnerFrankLiu <enginnerFrankLiu@icloud.com>
2021-05-24 23:05:08 +0800

    log one hour.

--- learn/src/Main.java
+++ learn/src/Main.java
@@ -4,10 +4,12 @@ public class Main {
         System.out.println("hello java.");
         System.out.println("fuck the java.");
     }
+    public void info(){
+        System.out.println("hello for....learn one hour log.");
+    }
     public static void main(String[] args) throws Exception {
         System.out.format("application start.").println();
         helloInfo();
         System.out.format("application end.");
     }
-
 }
